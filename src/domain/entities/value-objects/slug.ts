e2190b7cff1b55fc2d2ba7e4fc8d0b70016dfa05
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-05-23 12:11:06 -0300

    Fundamentos DDD - Value Object de slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+    public value: string
+
+    constructor(value: string) {
+        this.value = value
+    }
+
+    /**
+     * Receives a string and normalize it as a slug.
+     * 
+     * Example: "An example title" => "an-example-title"
+     * 
+     * @param text {string}
+     */
+    static createFromText(text: string): Slug {
+        const slugText = text
+        .normalize('NFKD')
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-') // replace every white space
+        .replace(/[^\w-]+/g, '') // replace everything that are not words
+        .replace(/_/g, '-') // replace every underline
+        .replace(/--+/g, '-') // replace every double "--"
+        .replace(/-$/g, '') // replace every text that ends with "-"
+
+        return new Slug(slugText)
+    }
+}
