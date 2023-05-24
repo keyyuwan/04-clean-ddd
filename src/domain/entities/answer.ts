d3dcd4c0250bb9f5035cf67f41013ef5716cf273
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-05-24 09:31:02 -0300

    Fundamentos de DDD - Abstraindo criação de entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,12 +1,28 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface AnswerProps {
-    content: string, authorId: string, questionId: string
+    content: string, 
+    authorId: UniqueEntityID, 
+    questionId: UniqueEntityID
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
     // getter -> prop can be accessed externaly
     get content() {
         return this.props.content
+    }
+
+    // this function will have the role of the Entity constructor (super)
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date()
+        }, id)
+
+        return answer
     }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,25 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
-    title: string, slug: Slug, content: string, authorId: string
+    title: string, 
+    slug: Slug, 
+    content: string, 
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
+    static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+        const question = new Question({
+            ...props,
+            createdAt: new Date()
+        }, id)
 
+        return question
+    }
 }
