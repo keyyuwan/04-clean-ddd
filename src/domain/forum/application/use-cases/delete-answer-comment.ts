a0df930a216cf89e8f2e3fe02c43744334891175
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-06-01 09:32:16 -0300

    feat(domain): delete answer comment

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -0,0 +1,33 @@
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+interface DeleteAnswerCommentUseCaseRequest {
+  answerCommentId: string
+  authorId: string
+}
+
+interface DeleteAnswerCommentUseCaseResponse {}
+
+export class DeleteAnswerCommentUseCase {
+  constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    answerCommentId,
+    authorId,
+  }: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
+    const answerComment = await this.answerCommentsRepository.findById(
+      answerCommentId,
+    )
+
+    if (!answerComment) {
+      throw new Error('Comment not found.')
+    }
+
+    if (authorId !== answerComment.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answerCommentsRepository.delete(answerComment)
+
+    return {}
+  }
+}
