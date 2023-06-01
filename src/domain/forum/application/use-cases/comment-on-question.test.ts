0e34592916aa44765f099a4926054540257ccd58
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-06-01 08:52:02 -0300

    feat: comment on question

---
+++ src/domain/forum/application/use-cases/comment-on-question.test.ts
@@ -0,0 +1,36 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment On Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      authorId: 'author-1',
+      questionId: question.id.toString(),
+      content: 'Question comment content',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Question comment content',
+    )
+  })
+})
